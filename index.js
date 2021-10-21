91cba9bd925bc6c900d392dbe1bfc5e56dc4eef7
Ido Strauss <strauss02@hotmail.co.il>
2021-10-21 16:30:40 +0300

    add timeFromAtoB function

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function timeToNow(date) {
   const dateMoment = moment(date);
   return moment(dateMoment, 'YYYYMMDD').fromNow();
 }
+
+function timeFromAToB(date1, date2) {
+  const date1Moment = moment(date1);
+  const date2Moment = moment(date2);
+
+  return date1Moment.diff(date2Moment, 'years');
+}
